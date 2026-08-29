824a8d73ce7db25a47133f8963cfab1608c95450
Eduardo França <dudisfranca@gmail.com>
2026-08-28 21:52:30 -0300

    KI-OFERTA

---
+++ dominio/mercado.py
@@ -0,0 +1,17 @@
+class Mercado:
+    def __init__(self, id, nome, lat, log):
+        
+        self._id = id
+        self._nome = nome
+        self._lat = lat
+        self._log = log
+
+    def mostrar_nome_mercado(self):
+        pass 
+    
+    def alterar_lat(self, nova_lat):
+        self._
+        if nova_lat 
+
+
+
